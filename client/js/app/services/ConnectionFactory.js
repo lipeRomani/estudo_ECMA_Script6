3ef2eca29426644f59a2cd0505c4cf2e55f4c6a6
Felipe Augusto Romani <felipearomani@MacBook-Pro-de-Felipe.local>
2017-05-02 07:32:11 -0300

    monkey pathing e module pattern

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,41 +1,62 @@
-var version = 5;
-var dbName = 'aluraframe';
-var stores = ['negociacoes'];
 
-class ConnectionFactory {
+var ConnectionFactory = (function(){
+    const version = 5;
+    const dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    
+    var connection;
+    var close;
 
-    constructor() {
-        throw new Error("Esta classe não pode ser instanciada.");
-    }
+    return class ConnectionFactory {
 
-    static getConnection() {
-        
-        return new Promise((resolve, reject) => {
-            let requestOpen = window.indexedDB.open(dbName);
+        constructor() {
+            throw new Error("Esta classe não pode ser instanciada.");
+        }
+
+        static getConnection() {
             
-            requestOpen.onupgradeneeded = event => {
-                ConnectionFactory._createStores(event.target.result);
-            }
-            
-            requestOpen.onsuccess = event => {
-                resolve(resolve);
-            }
+            return new Promise((resolve, reject) => {
+                let requestOpen = window.indexedDB.open(dbName);
+                
+                requestOpen.onupgradeneeded = event => {
+                    ConnectionFactory._createStores(event.target.result);
+                }
+                
+                requestOpen.onsuccess = event => {
+                    if (!connection) {
+                        connection = event.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = () =>{
+                            throw new Error("Favor encerrar a conexão pelo metodo ConnectionFactory.__close()");
+                        }
+                    }
+                    resolve(connection);
+                }
 
-            requestOpen.onerror = event => {
-                console.log(event.target.error);
-                reject(event.target.error.name);
+                requestOpen.onerror = event => {
+                    console.log(event.target.error);
+                    reject(event.target.error.name);
+                }
+            }); 
+
+        }
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);    
+                }
+                connection.createObjectStore(store, {"autoIncrement" : true});
+            });
+        }
+
+        static close() {
+            if (connection) {
+                close();
+                connection = null;
             }
-        }); 
+        }
 
     }
+})();
 
-    static _createStores(connection) {
-        stores.forEach(store => {
-            if (connection.objectStoreNames.contains(store)) {
-                connection.deleteObjectStore(store);    
-            }
-            connection.createObjectStore(store, {"autoIncrement" : true});
-        });
-    }
-
-}
